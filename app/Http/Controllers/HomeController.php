bb557d63aae765391d60f5887938c34806f43201
infomotin <touch.u.bc1000@gmail.com>
2023-09-26 08:44:23 +0600

    wasChanged

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,28 +30,45 @@ class HomeController extends Controller
             ]
         );
         echo $user->id . '-' . $user->name;
+        echo '<br>';
+        echo $user->wasRecentlyCreated?'Created':'Found';
+
+        $user->name = 'motin Update';
+        echo $user->isDirty() ? 'Dirty' : 'Clean';
+        echo '<br>';
+        echo $user->isDirty('name') ? 'name' : 'Not Editable';
+        echo '<br>';
+        // $user->email = 'motin@gmail.com';
+        echo $user->isDirty('email') ? 'email' : 'Not Editable';
+
+
         //updateOrCreate
-        $user = User::updateOrCreate(
-            ['email' => 'motin@admin.com'],
-            [
-                'name' => 'motin Update',
-                'password' => bcrypt('654321')
-            ]
-        );
+        // $user = User::updateOrCreate(
+        //     ['email' => 'motin@admin.com'],
+        //     [
+        //         'name' => 'motin Update',
+        //         'password' => bcrypt('654321')
+        //     ]
+        // );
         
-        echo $user->id . '-' . $user->name;
+        // echo $user->id . '-' . $user->name;
         //user upsert
-        User::upsert(
-            [
-                ['email' => 'motin1@admin.com', 'name' => 'motin Update', 'password' => bcrypt('654321')],
-                ['email' => 'admin1@admin.com', 'name' => 'admin', 'password' => bcrypt('123456')],
-            ],
-            [
-                'name','password','email'
-            ]
-        );
-        foreach(User::all() as $user){
-            echo $user->id . '<br>' .  $user->name . '<br>' . $user->email;
-        }
+        // User::upsert(
+        //     [
+        //         ['email' => 'motin1@admin.com', 'name' => 'motin Update', 'password' => bcrypt('654321')],
+        //         ['email' => 'admin1@admin.com', 'name' => 'admin', 'password' => bcrypt('123456')],
+        //     ],
+        //     ['email'],['name','password']
+        // );
+        // foreach(User::all() as $user){
+        //     // echo $user->id . '<br>' .  $user->name . '<br>' . $user->email;
+        // }
+        $user->save();
+        echo $user->wasChanged() ? 'Changed' : 'Not Changed';
+        echo '<br>';
+        echo $user->isDirty() ? 'Dirty' : 'Clean';
+        echo '<br>';
+        echo $user->wasChanged('name') ? 'name' : 'Not Changed';
+        
     }
 }
